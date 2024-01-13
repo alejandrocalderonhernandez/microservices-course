36ae8b6b690decd64556f7e708aaa332d0d94e15
alejandrocalderonhernandez <alejandro950825@gmail.com>
2024-01-13 13:19:45 -0600

    02-eureka-discovery-registry

--- companies-crud/src/main/java/com/debuggeandoideas/companiescrud/CompaniesCrudApplication.java
+++ companies-crud/src/main/java/com/debuggeandoideas/companiescrud/CompaniesCrudApplication.java
@@ -2,8 +2,10 @@ package com.debuggeandoideas.companiescrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CompaniesCrudApplication {
 
 	public static void main(String[] args) {
---
+++ registry-server/src/test/java/com/debuggeandoideas/registryserver/RegistryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debuggeandoideas.registryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
